526ff85fe86e8c4329310f757af33e54bb9d0c81
ildevelop <raduilya@gmail.com>
2024-08-17 15:19:22 +0300

    [fix] clean code

--- src/App.tsx
+++ src/App.tsx
@@ -12,47 +12,7 @@ import { UserProvider } from './context/UserContext';
 import ErrorBoundary from './components/ErrorBoundary';
 import LoadingSpinner from './components/LoadingSpinner';
 
-// const fsm = new FSM('idle', {
-//   idle: { on: { FETCH: 'loading' } },
-//   loading: { on: { SUCCESS: 'processing', FAILURE: 'error' } },
-//   processing: { on: { SUCCESS: 'success', FAILURE: 'error' } },
-//   success: { on: { RESET: 'idle' } },
-//   error: { on: { RETRY: 'loading' } },
-// });
-
 const App: React.FC = () => {
-  // const [state, setState] = useState(fsm.getState());
-  // const [data, setData] = useState([]);
-
-  // const handleEvent = (event: string) => {
-  //   try {
-  //     fsm.send(event);
-  //     setState(fsm.getState());
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (state === 'loading') {
-  //     axios
-  //       .get('https://jsonplaceholder.typicode.com/posts')
-  //       .then((response) => {
-  //         setData(response.data);
-  //         handleEvent('SUCCESS');
-  //       })
-  //       .catch((error) => {
-  //         console.error('Error fetching data:', error);
-  //         handleEvent('FAILURE');
-  //       });
-  //   }
-  //   if (state === 'processing') {
-  //     setTimeout(() => {
-  //       handleEvent('SUCCESS');
-  //     }, 1000);
-  //   }
-  // }, [state]);
-
   return (
     <UserProvider>
       <Router>
@@ -67,27 +27,6 @@ const App: React.FC = () => {
             </Routes>
           </Suspense>
         </ErrorBoundary>
-        {/* <h1>Current State: {state}</h1>
-      {state === 'idle' && <button onClick={() => handleEvent('FETCH')}>Fetch Posts</button>}
-      {state === 'loading' && <p>Loading...</p>}
-      {state === 'processing' && <p>Processing...</p>}
-      {state === 'success' && (
-        <div>
-          <h2>Posts:</h2>
-          <ul>
-            {data.map((post: { id: number; title: string }) => (
-              <li key={post.id}>{post.title}</li>
-            ))}
-          </ul>
-          <button onClick={() => handleEvent('RESET')}>Reset</button>
-        </div>
-      )}
-      {state === 'error' && (
-        <div>
-          <p>Error occurred while fetching data</p>
-          <button onClick={() => handleEvent('RETRY')}>Retry</button>
-        </div>
-      )} */}
       </Router>
     </UserProvider>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/fsm-react/',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/fsm-react/' : '/',
+  };
 });
